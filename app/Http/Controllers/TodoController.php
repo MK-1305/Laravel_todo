7552c0433749e981ac22ef50c48ccf23980df120
松浦一樹 <matsuurakazuki@matsuurakazukinoMacBook-Air.local>
2024-04-08 00:01:23 +0900

    編集と削除機能実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,19 +9,40 @@ class TodoController extends Controller
 {
     public function index() {
         $todos = Todo::all();
-
         return view('todos.index', compact('todos'));
     }
     public function new() {
         return view('todos.new');
     }
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validatedData = $request->validate([
             'title' => 'required|max:255',
+        ], [
+            'title.required' => 'タスクのタイトルを入力してください。',
         ]);
 
         Todo::create($validatedData);
 
         return redirect()->route('todos.index')->with('success', 'タスクが追加されました。');
+    }
+    public function edit($id) {
+        $todo = Todo::findOrFail($id);
+        return view('todos.edit', compact('todo'));
+    }
+    public function update(Request $request, $id) {
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $todo = Todo::findOrFail($id);
+        $todo->update($validatedData);
+
+        return redirect()->route('todos.index')->with('success', 'タスクが更新されました。');
+    }
+    public function destroy($id) {
+        $todo = Todo::findOrFail($id);
+        // dd($todo);
+        $todo->delete();
+        return redirect()->route('todos.index')->with('success', 'タスクが削除されました。');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ Route::get('/', [TodoController::class, 'index'])->name('todos.index');
 Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
 Route::get('/todos/new', [TodoController::class, 'new'])->name('todos.new');
 Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
+Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
